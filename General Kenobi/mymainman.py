36fe6af3bc78005d60e68fb5ab14a487dafbb2b0
Noneya <dyllan.oberzan@nelnet.net>
2025-08-06 16:30:13 +0000

    This is dumb

--- General Kenobi/mymainman.py
+++ General Kenobi/mymainman.py
@@ -2,5 +2,5 @@ import webbrowser
 import os
 
 # Open the HTML file in the default browser
-file_path = os.path.abspath("Hello There.html")
+file_path = os.path.abspath("General Kenobi/index.html")
 webbrowser.open(f"file://{file_path}")
